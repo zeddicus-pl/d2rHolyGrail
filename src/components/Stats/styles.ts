0e0af955f50be7d6ad8f4c8ce2fe36f476f8789c
zeddicus-pl <kamil.tunkiewicz [at] gmail.com>
2022-02-18 14:10:10 +0100

    some last visual fixes

--- src/components/Stats/styles.ts
+++ src/components/Stats/styles.ts
@@ -13,7 +13,7 @@ export const ImageContainer = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -70%);
   text-align: center;
 `
 
