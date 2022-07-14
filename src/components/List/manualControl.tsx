5c47c9339ecca64d4fa713b6e3e5dbe1c6340d24
Kamil Tunkiewicz <ktunkiewicz@hellozai.com>
2022-07-14 10:24:20 +0200

    fixes to ethereal items, counting manual items

---
+++ src/components/List/manualControl.tsx
@@ -0,0 +1,69 @@
+import { Button, Checkbox, FormControl } from "@mui/material";
+import { ChangeEvent, MouseEvent } from "react";
+import { ItemsInSaves } from "../../@types/main";
+/* @ts-ignore */
+import CheckBoxOutlineBlank from '@mui/material/internal/svg-icons/CheckBoxOutlineBlank';
+import { CheckboxCounter, CheckboxCounterButtons } from "./styles";
+
+type ManualControlProps = {
+    isPlaceholder?: boolean,
+    items: ItemsInSaves,
+    itemName: string,
+    className?: string,
+    onChange: (event: ChangeEvent<HTMLInputElement>, itemName: string) => void,
+    onNumberChange: (event: MouseEvent<HTMLButtonElement>, itemName: string, count: number) => void,
+}
+const ManualControl = ({ isPlaceholder, items, itemName, className, onChange, onNumberChange }: ManualControlProps) => {
+    if (isPlaceholder) {
+        return <div style={{ width: 30, display: 'inline-block' }}></div>;
+    }
+    let count = 0;
+
+    if (items[itemName] && items[itemName].inSaves) {
+        count = Object.values(items[itemName].inSaves).reduce(
+            (acc, itemsInSave) => acc + itemsInSave.length,
+            0
+        );
+    }
+
+    // previous manual items save format does not store the "inSaves" so we assume that's 1 item
+    if (items[itemName] && count == 0) {
+        count = 1;
+    }
+    
+    return <FormControl className="checkboxWithButtons">
+        <Checkbox
+            className={className}
+            edge="start"
+            checked={!!items[itemName]}
+            tabIndex={-1}
+            checkedIcon={count > 1 ? <>
+                <CheckBoxOutlineBlank />
+                <CheckboxCounter>{count}</CheckboxCounter>
+            </> : undefined}
+            disableRipple
+            onChange={(e) => onChange(e, itemName)}
+            onClick={(e: MouseEvent<HTMLButtonElement>) => { e.stopPropagation() }}
+        />
+        <CheckboxCounterButtons>
+            {count > 0 &&
+                <Button
+                    variant="contained"
+                    size="small"
+                    color="primary"
+                    onClick={(e) => onNumberChange(e, itemName, count + 1)}
+                >+</Button>
+            }
+            {count > 1 &&
+                <Button
+                    variant="contained"
+                    size="small"
+                    color="primary"
+                    onClick={(e) => onNumberChange(e, itemName, count - 1)}
+                >-</Button>
+            }
+        </CheckboxCounterButtons>
+    </FormControl>
+}
+
+export default ManualControl;
